da6e2675c7c20d706875c05cb8688ab88b856544
Carlos Lagos Urbina <vagrant@wheezy.raw>
2017-07-06 00:03:45 +0000

    Commit Inicial

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;dbname=dsigrupo1',
+    'username' => 'dsiggrupo1',
+    'password' => 'dsi20171',
+    'charset' => 'utf8',
+];
