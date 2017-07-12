5d414b942bef6c25a818c58c066e95e580800586
JavierRD14 <javier0194@gmail.com>
2017-07-12 14:29:43 -0400

    'commitinicial'

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;dbname=DSI',
+    'username' => 'postgres',
+    'password' => 'admin123',
+    'charset' => 'utf8',
+];
