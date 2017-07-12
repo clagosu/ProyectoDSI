10df93af3b8b84fdd8c9a08adaecf67da12a0975
ymoena <ymoena4@gmail.com>
2017-07-12 16:47:14 -0400

    commit_Rules_Convenio
    Se agregaron las restricciones para los atributos de la tabla covenio

--- models/Convenio.php
+++ models/Convenio.php
@@ -37,12 +37,31 @@ class Convenio extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['id_convenio', 'id_tipo_convenio', 'id_estado_convenio'], 'required'],
+            [['id_convenio', 'id_tipo_convenio', 'id_estado_convenio','id_coordinador_convenio'], 'required'],
             [['id_convenio', 'id_tipo_convenio', 'id_estado_convenio', 'numero_decreto', 'vigencia'], 'integer'],
             [['fecha_inicio', 'fecha_termino', 'fecha_firma', 'fecha_decreto'], 'safe'],
             [['id_coordinador_convenio'], 'string', 'max' => 20],
-            [['nombre_convenio', 'descripcion'], 'string', 'max' => 500],
-            [['vigente'], 'string', 'max' => 1],
+            [['nombre_convenio', 'descripcion', 'area'], 'string', 'max' => 500],
+            [['vigente'], 'string', 'max' => 2],
+            [['id_coordinador_convenio'], 'exist', 'skipOnError' => true, 'targetClass' => Coordinador::className(), 'targetAttribute' => ['id_coordinador_convenio' => 'id_coordinador_convenio']],
+            [['id_estado_convenio'], 'exist', 'skipOnError' => true, 'targetClass' => Estado::className(), 'targetAttribute' => ['id_estado_convenio' => 'id_estado_convenio']],
+                   
+              [['fecha_inicio', 'fecha_termino', 'fecha_firma', 'fecha_decreto'], 'date', 'format'=>'yyyy-mm-dd'],
+                    [['id_coordinador_convenio','id_convenio','id_tipo_convenio','id_estado_convenio'],'match', 'pattern' => "/^[0-9A-Z]+$/", 'message' => 'Sólo se aceptan letras Mayusculas y números enteros'],
+                    [['nombre_convenio', 'descripcion', 'area'],'match', 'pattern' => '/^[a-záéíóúñ\s]+$/i', 'message' => 'Sólo se aceptan letras'],
+          
+              ['fecha_termino', 'compare', 'compareAttribute' => 'fecha_inicio', 'operator' => '>'],
+            ['fecha_termino', 'compare', 'compareAttribute' => 'fecha_firma', 'operator' => '>'],
+            ['fecha_termino', 'compare', 'compareAttribute' => 'fecha_decreto', 'operator' => '>'],
+            
+               ['fecha_inicio', 'compare', 'compareAttribute' => 'fecha_termino', 'operator' => '<'],
+            ['fecha_inicio', 'compare', 'compareAttribute' => 'fecha_firma', 'operator' => '<'],
+            ['fecha_inicio', 'compare', 'compareAttribute' => 'fecha_decreto', 'operator' => '<'],
+            
+            ['vigente','match', 'pattern' => '/^S(i)|N(o)|\s/', 'message' => 'Sólo se acepta "Si" o "No"'],
+            ['id_convenio','unique'],
+
+            
         ];
     }
 
