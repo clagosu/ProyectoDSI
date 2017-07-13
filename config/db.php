6cd48dfce353c49935c53e15ac4fd1975c5ec01c
rooliva <rooliva@alumnos.ubiobio.cl>
2017-07-13 14:55:50 -0400

    Commit Inicial

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;dbname=dsi2017',
+    'username' => 'postgres',
+    'password' => 'udeconce',
+    'charset' => 'utf8',
+];
